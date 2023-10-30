fe37b8da2008bfbbfc78d584e174f06c55c95699
Lorena Medeiros <lomedeiros@proton.me>
2023-10-30 16:46:18 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(22)2222-3333", modelo: "Nokia G60", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "(33)3333-4444", modelo: "Iphone 13", imei: "2222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
